9aa24ab3b41f4db79e13313aa694a8b5694b4865
WFranklin626 <157049202+WFranklin626@users.noreply.github.com>
2024-01-24 15:22:38 -0800

    Add files via upload

---
+++ Franklin_SalesTacCalc_Project/Franklin_SalesTacCalc_Project/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Franklin_SalesTacCalc_Project
+{
+    partial class frmDealer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbWarranty = new System.Windows.Forms.ComboBox();
+            this.cmbState = new System.Windows.Forms.ComboBox();
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.lblBase = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lblWarranty = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblDealer = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbWarranty
+            // 
+            this.cmbWarranty.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.cmbWarranty.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbWarranty.FormattingEnabled = true;
+            this.cmbWarranty.Items.AddRange(new object[] {
+            "No Warranty",
+            "One Year",
+            "Two Year",
+            "Three Year"});
+            this.cmbWarranty.Location = new System.Drawing.Point(210, 158);
+            this.cmbWarranty.Name = "cmbWarranty";
+            this.cmbWarranty.Size = new System.Drawing.Size(205, 33);
+            this.cmbWarranty.TabIndex = 0;
+            // 
+            // cmbState
+            // 
+            this.cmbState.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.cmbState.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbState.FormattingEnabled = true;
+            this.cmbState.Items.AddRange(new object[] {
+            "",
+            "WA",
+            "OR"});
+            this.cmbState.Location = new System.Drawing.Point(210, 219);
+            this.cmbState.Name = "cmbState";
+            this.cmbState.Size = new System.Drawing.Size(92, 33);
+            this.cmbState.TabIndex = 1;
+            // 
+            // txtBase
+            // 
+            this.txtBase.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtBase.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBase.Location = new System.Drawing.Point(210, 97);
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(205, 30);
+            this.txtBase.TabIndex = 2;
+            // 
+            // lblBase
+            // 
+            this.lblBase.AutoSize = true;
+            this.lblBase.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBase.Location = new System.Drawing.Point(12, 102);
+            this.lblBase.Name = "lblBase";
+            this.lblBase.Size = new System.Drawing.Size(198, 25);
+            this.lblBase.TabIndex = 3;
+            this.lblBase.Text = "Vehicle Base Price: $";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalc.Location = new System.Drawing.Point(382, 212);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(142, 45);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // lblWarranty
+            // 
+            this.lblWarranty.AutoSize = true;
+            this.lblWarranty.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWarranty.Location = new System.Drawing.Point(12, 161);
+            this.lblWarranty.Name = "lblWarranty";
+            this.lblWarranty.Size = new System.Drawing.Size(186, 25);
+            this.lblWarranty.TabIndex = 5;
+            this.lblWarranty.Text = "Extended Warranty:";
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblState.Location = new System.Drawing.Point(89, 222);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(109, 25);
+            this.lblState.TabIndex = 6;
+            this.lblState.Text = "State Sold:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(382, 419);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(111, 62);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(517, 419);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(114, 62);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblDealer
+            // 
+            this.lblDealer.AutoSize = true;
+            this.lblDealer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealer.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblDealer.Location = new System.Drawing.Point(79, 35);
+            this.lblDealer.Name = "lblDealer";
+            this.lblDealer.Size = new System.Drawing.Size(491, 31);
+            this.lblDealer.TabIndex = 9;
+            this.lblDealer.Text = "Billy Bob\'s Vehicle Sales Tax Calculator";
+            this.lblDealer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(135, 317);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(0, 25);
+            this.lblTotal.TabIndex = 10;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(162, 358);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 11;
+            this.lblError.Visible = false;
+            // 
+            // frmDealer
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(636, 496);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblDealer);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.lblWarranty);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lblBase);
+            this.Controls.Add(this.txtBase);
+            this.Controls.Add(this.cmbState);
+            this.Controls.Add(this.cmbWarranty);
+            this.MaximizeBox = false;
+            this.Name = "frmDealer";
+            this.Text = "Billy Bob\'s Vehicle Sales Tax Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbWarranty;
+        private System.Windows.Forms.ComboBox cmbState;
+        private System.Windows.Forms.TextBox txtBase;
+        private System.Windows.Forms.Label lblBase;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lblWarranty;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblDealer;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblError;
+    }
+}
+
---
+++ Franklin_SalesTacCalc_Project/Franklin_SalesTacCalc_Project/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Franklin_SalesTacCalc_Project
+{
+    public partial class frmDealer : Form
+    {
+        public frmDealer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            // Get base cost from textbox
+            if (decimal.TryParse(txtBase.Text, out decimal baseCost))
+            {
+                // Get selected warranty
+                decimal warrantyCost = 0;
+                switch (cmbWarranty.SelectedIndex)
+                {
+                    case 1:
+                        warrantyCost = 1000;
+                        break;
+                    case 2:
+                        warrantyCost = 2000;
+                        break;
+                    case 3:
+                        warrantyCost = 3000;
+                        break;
+                }
+
+                // Get selected state
+                decimal salesTaxRate = 0;
+                switch (cmbState.SelectedItem.ToString())
+                {
+                    case "WA":
+                        salesTaxRate = 0.086m;
+                        break;
+                    case "OR":
+                        salesTaxRate = 0;
+                        break;
+                }
+
+                // Calculate total cost including sales tax and warranty cost
+                decimal totalCost = baseCost + warrantyCost + (baseCost * salesTaxRate);
+
+                // Display total cost in label
+                lblTotal.Text = $"Total Vehicle cost is {totalCost:C}";
+
+                // Clear error label
+                lblError.Text = string.Empty;
+            }
+            else
+            {
+                // Show error message in lblError
+                lblError.Text = "Please enter a valid base cost.";
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Clear all controls and set focus to base cost textbox
+            txtBase.Clear();
+            cmbWarranty.SelectedIndex = 0;
+            cmbState.SelectedIndex = 0;
+            lblTotal.Text = string.Empty;
+            lblError.Text = string.Empty;  // Clear error label
+
+            // Remove focus from cmbState
+            this.ActiveControl = txtBase;
+        }
+    }
+}
